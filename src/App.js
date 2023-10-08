ba1b4c7f9455540cee7684e848f2024a2bdd2c06
Immanuel Williams <l0calh34rtz@gmail.com>
2023-10-08 16:54:03 +0700

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ])
+}
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Cat",
+      breed: "British Shorthair",
+    }),
+  ])
+}
+
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(React.createElement(App))
